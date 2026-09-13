8978b0135db7b1923ea0f4892fe641510f2a65de
Niko <niko-meuronen@hotmail.com>
2026-09-13 13:50:50 +0300

    testi

--- Moduuli 3.py/1.py
+++ Moduuli 3.py/1.py
@@ -1,6 +1,6 @@
-nimi = input ("Mikä on nimesi?")
-if nimi == "Matti":
-    print ("Seuraava, kiitos !")
-else
-    print ("Montako keittoannosta haluat?")
-    
+#Kuhan pituus
+print ("Kuinka pitkä kuha on?")
+kuhan_pituus = float(input("Syötä kuhan pituus cm: "))
+if kuhan_pituus <37:
+    print ("Kuha on alamittainen, ja se on päästettävä takaisin järveen. Kuha on " + str(37 - kuhan_pituus) + " cm liian lyhyt.")
+     
